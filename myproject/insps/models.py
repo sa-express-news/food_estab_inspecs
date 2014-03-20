01c3258b7f132016bcfbe73396e3a3064e4733bb
Joseph Kokenge <jkokenge@gmail.com>
2014-03-20 12:48:13 -0500

    removes model definition for geocoded estabs

--- myproject/insps/models.py
+++ myproject/insps/models.py
@@ -1,8 +1,4 @@
 from django.db import models
 
-class GeocodedEstab(models.Model):
-	name = models.CharField(max_length=255)
-	address = models.CharField(max_length=255)
-	latitude = models.DecimalField(max_digits=14, decimal_places=10)
-	longitude = models.DecimalField(max_digits=14, decimal_places=10)
+
 
