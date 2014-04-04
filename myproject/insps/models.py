0cddb7946fec966e3388552a2ea936a30c9eb4ad
Joseph Kokenge <jkokenge@gmail.com>
2014-04-04 11:09:32 -0500

    adds date field to insps_inspection table

--- myproject/insps/models.py
+++ myproject/insps/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 class GeocodedEstab(models.Model):
 	name = models.CharField(max_length=255)
-	estab_id = models.IntegerField(max_length=12, null=True)
+	estab_id = models.IntegerField(max_length=12, null=True, unique=True)
 	address = models.CharField(max_length=255)
 	latitude = models.DecimalField(max_digits=10, decimal_places=7)
 	longitude = models.DecimalField(max_digits=10, decimal_places=7)
 
 
-# class Inspection(models.Model):
-# 	estab_id = models.ForeignKey(GeocodedEstab, to_field='estab_id')
-# 	demerits = models.CharField(max_length=255)
+class Inspection(models.Model):
+	estab_id = models.ForeignKey(GeocodedEstab, to_field='estab_id')
+	demerits = models.CharField(max_length=255, null=True)
+	demerits_nums = models.IntegerField(max_length=5, null=True)
+	inspection_key = models.CharField(max_length=255, null=True)
+	date = models.DateField(auto_now=False, auto_now_add=False, null=True) 
 
 
 
