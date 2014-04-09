d500ce368955149148391a763104b535ff29801d
Joseph Kokenge <jkokenge@gmail.com>
2014-04-09 18:57:48 -0500

    adds insps_desciption model and table

--- myproject/insps/models.py
+++ myproject/insps/models.py
@@ -7,13 +7,21 @@ class GeocodedEstab(models.Model):
 	latitude = models.DecimalField(max_digits=10, decimal_places=7)
 	longitude = models.DecimalField(max_digits=10, decimal_places=7)
 
+	def __unicode_(self):
+		return self.name, self.address
+
 
 class Inspection(models.Model):
 	estab_id = models.ForeignKey(GeocodedEstab, to_field='estab_id')
 	demerits = models.CharField(max_length=255, null=True)
 	demerits_nums = models.IntegerField(max_length=5, null=True)
-	inspection_key = models.CharField(max_length=255, null=True)
-	date = models.DateField(auto_now=False, auto_now_add=False, null=True) 
+	inspection_key = models.CharField(max_length=255, null=True, unique=True)
+	date = models.DateField(auto_now=False, auto_now_add=False, null=True)
+
+class Description(models.Model):
+	estab_id = models.ForeignKey(GeocodedEstab, to_field='estab_id')
+	inspection_key = models.ForeignKey(Inspection, to_field='inspection_key')
+	viol_text = models.TextField(null=True)
 
 
 
