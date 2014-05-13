fe605c1c85d03cc9319515d6247021e52e47997c
Joseph Kokenge <jkokenge@gmail.com>
2014-05-13 15:13:36 -0500

    orders estab view by inspection date, descending, i.e. most recent at the top

--- myproject/insps/views.py
+++ myproject/insps/views.py
@@ -1,7 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+
+from django.shortcuts import render
+
+from insps.models import Inspection, Description
 
 def index(request):
-    return HttpResponse("This is the restaurant inspections index.")
+    latest_insps = Inspection.objects.filter(date__range=["2013-11-30", "2013-12-31"])
+    template = loader.get_template('insps/index.html')
+    context = {'latest_insps': latest_insps}
+    return render(request, 'insps/index.html', context)
 
-def detail(request, estab_id):
-    return HttpResponse("Detail page for restaurant of estab_id: %s." % estab_id)
+def estab(request, estab_id):
+    try:
+        estab = Inspection.objects.all().filter(estab_id=estab_id).order_by('-date')
+    except Inspection.DoesNotExist:
+        raise Http404
+    return render(request, 'insps/estab.html', { 'estab': estab })
+
+
+def inspection(request, inspection_key):
+    return HttpResponse("These are the details for this date's inspection of this restaurant %s" % inspection_key)
