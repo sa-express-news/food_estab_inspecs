30db9dbafc0aa6f2bcc3335c9affbf657e2ba6d5
Joseph Kokenge <jkokenge@gmail.com>
2014-05-13 12:16:23 -0500

    changes architecture from index->detail to index->estab views, so this changes urls.py from details to estab

--- myproject/insps/urls.py
+++ myproject/insps/urls.py
@@ -3,5 +3,5 @@ from insps import views
 
 urlpatterns = patterns('', 
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<estab_id>\d+)/$', views.detail, name='details'),
+    url(r'^(?P<estab_id>\d+)/$', views.estab, name='estab'),
     )
