3265c1b69500f97c5f911ef70edda74ccd3e91d0
Joseph Kokenge <jkokenge@gmail.com>
2014-05-14 10:41:09 -0500

    urls now are estab_id for each estab page and inspection_key for inspection.html--unable to get the estab_id, estab_id/inspection_key nested url to work

--- myproject/insps/urls.py
+++ myproject/insps/urls.py
@@ -4,4 +4,5 @@ from insps import views
 urlpatterns = patterns('', 
     url(r'^$', views.index, name='index'),
     url(r'^(?P<estab_id>\d+)/$', views.estab, name='estab'),
+    url(r'^(?P<inspection_key>\d+-\d+\/\d+\/\d+)/$', views.inspection, name='inspection'),
     )
