078ddce551d91a0318a6e80259130d254194e4c4
Joseph Kokenge <jkokenge@gmail.com>
2014-09-10 12:06:46 -0500

    working draft of insps importer finished

---
+++ myproject/insps_importer.py
@@ -0,0 +1,48 @@
+import urllib2
+import csv
+import datetime
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+from insps.models import Inspection
+
+BASEURL = 'https://s3.amazonaws.com/inspections-csvs/'
+
+test_today = '08_26_14'
+
+def make_today():
+    today = datetime.datetime.now().strftime("%m_%d_%y")
+    return today
+
+def load(reader_o):
+    inspections = Inspection.objects.all()
+    print(len(inspections))
+
+    for o in reader_o:
+        record = Inspection(**o)
+        record_exists = inspections.filter(inspection_key=record.inspection_key).exists()
+        if record_exists:
+            print("record exists")
+
+        else:
+            print("new record")
+            record.save()
+            
+    inspections = Inspection.objects.all()
+    print(len(inspections))
+
+
+def get_csv(filename):
+    url = BASEURL + test_today + '/' + filename
+
+    response = urllib2.urlopen(url)
+    reader_o = csv.DictReader(response)
+    return load(reader_o)
+
+
+def load_csv():
+    get_csv('inspections_tbl.csv')
+
+
+if __name__ == '__main__':
+    load_csv()
