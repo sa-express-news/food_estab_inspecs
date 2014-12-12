1f3c8e35286365296b0a7b95f82d15ac79340ff6
Joseph Kokenge <jkokenge@gmail.com>
2014-12-12 15:11:40 -0600

    changed urls.py to reflect different structure of the inspection key

--- myproject/insps/urls.py
+++ myproject/insps/urls.py
@@ -4,5 +4,5 @@ from insps import views
 urlpatterns = patterns('', 
     url(r'^$', views.index, name='index'),
     url(r'^(?P<estab_id>\d+)/$', views.estab, name='estab'),
-    url(r'^(?P<inspection_key>\d+-\d+\/\d+\/\d+)/$', views.inspection, name='inspection'),
+    url(r'^(?P<inspection_key>\d+-\d+_\d+_\d+)/$', views.inspection, name='inspection'),
     )
