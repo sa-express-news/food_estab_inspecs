624b75e1073f934916b8bcb84eb85982a390faf3
Joseph Kokenge <jkokenge@gmail.com>
2015-04-17 17:39:13 -0500

    updated for 4-12 inspection update

--- myproject/insps_importer.py
+++ myproject/insps_importer.py
@@ -34,7 +34,7 @@ def load(reader_o):
 
 def get_csv(filename):
     # url = BASEURL + make_today() + '/' + filename
-    url = BASEURL + "04_01_15" + '/' + filename
+    url = BASEURL + "04_12_15" + '/' + filename
 
     response = urllib2.urlopen(url)
     reader_o = csv.DictReader(response)
