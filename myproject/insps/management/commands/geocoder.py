c69cc63cb6a5c413379d9aac042468149bca2be6
Joseph Kokenge <jkokenge@gmail.com>
2015-04-29 12:47:14 -0500

    finished geocoder management command

---
+++ myproject/insps/management/commands/geocoder.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+from insps.models import GeocodedEstab
+from googlegeocoder import GoogleGeocoder
+import time
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        geocoder = GoogleGeocoder()
+
+        estabs_to_geocode = GeocodedEstab.objects.filter(latitude=None)
+        for estab in estabs_to_geocode:
+            print("Address to geocode: %s") % estab.address
+            try:
+                search = geocoder.get(estab.address)
+                time.sleep(1.5)
+                estab.latitude = search[0].geometry.location.lat
+                estab.longitude = search[0].geometry.location.lng
+                print("Latitude: %s, Longitude: %s \n") % (estab.latitude, estab.longitude)
+                estab.save()
+            except:
+                print(("Couldn't geocode."))
+        
