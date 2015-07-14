d8b0a2ea7c40b0df18b67af3900413677b8ac51b
Joseph Kokenge <jkokenge@gmail.com>
2015-07-14 10:05:04 -0500

    url for search form in urls.py looks to be ok

--- myproject/insps/urls.py
+++ myproject/insps/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<estab_id>\d+)/$', views.estab, name='estab'),
     url(r'^(?P<inspection_key>\d+-\d+_\d+_\d+)/$', views.inspection, name='inspection'),
+    url(r'^search/$', views.search, name='search'),
     )
