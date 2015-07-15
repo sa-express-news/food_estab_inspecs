bb0e557c3422ab156ab739287b4fb603b3094577
Joseph Kokenge <jkokenge@gmail.com>
2015-07-15 13:10:08 -0500

    added small description for search form

--- myproject/insps/forms.py
+++ myproject/insps/forms.py
@@ -1,4 +1,4 @@
 from django import forms 
 
 class SearchForm(forms.Form):
-    search_query = forms.CharField(required=True, max_length=100, widget=forms.TextInput(attrs={'class':'search-form'}))
+    search_query = forms.CharField(required=True, max_length=100, widget=forms.TextInput(attrs={'class':'search-input-box'}))
