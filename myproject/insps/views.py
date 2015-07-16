af4b3642e14d47d79c5e7019c16705d9fcd72259
Joseph Kokenge <jkokenge@gmail.com>
2015-07-16 15:44:06 -0500

    removed pagination, just went with datatables and no redirect, simple else statement in template handles no results returned condition

--- myproject/insps/views.py
+++ myproject/insps/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from insps.models import Inspection, Description, GeocodedEstab
 from insps.forms import SearchForm
@@ -38,16 +39,7 @@ def search(request):
         form = SearchForm(request.POST)
         if form.is_valid():
             s_query = form.cleaned_data['search_query']
-            results = GeocodedEstab.objects.all().filter(name__icontains=s_query)
-            paginator = Paginator(results, 10)
-            page = request.GET.get('page')
-
-            try: 
-                estabs = paginator.page(page)
-            except PageNotAnInteger:
-                estabs = paginator.page(1)
-            except EmptyPage:
-                estabs = paginator.page(paginator.num_pages)
+            estabs = GeocodedEstab.objects.all().filter(name__icontains=s_query)
 
             return render(request, 'insps/search.html', { 'form':form, 'estabs': estabs })
     else:
