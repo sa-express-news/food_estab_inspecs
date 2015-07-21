fcd20d6672bc9314e84996ac7ddab8e4a0a0ae19
Joseph Kokenge <jkokenge@gmail.com>
2015-07-21 14:44:26 -0500

    moved all the urls to the myproject conf

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
+from insps import views
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^insps/', include('insps.urls')),
+    url(r'^$', 'insps.views.index', name='index'),
+    url(r'^(?P<estab_id>\d+)/$', views.estab, name='estab'),
+    url(r'^(?P<inspection_key>\d+-\d+_\d+_\d+)/$', views.inspection, name='inspection'),
+    url(r'^search/$', views.search, name='search'),
     url(r'^admin/', include(admin.site.urls)),
 )
