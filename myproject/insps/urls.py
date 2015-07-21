79f08fc347bbe2bc834f5e6d4ab35b115ad1e05b
Joseph Kokenge <jkokenge@gmail.com>
2015-07-21 14:44:03 -0500

    cleaned out all the old urls, deleted file, urls just in myproject urls

--- myproject/insps/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from insps import views
-
-urlpatterns = patterns('', 
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<estab_id>\d+)/$', views.estab, name='estab'),
-    url(r'^(?P<inspection_key>\d+-\d+_\d+_\d+)/$', views.inspection, name='inspection'),
-    url(r'^search/$', views.search, name='search'),
-    )
