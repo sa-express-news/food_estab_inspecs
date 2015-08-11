2a301e6cf9b9108ccf78039a3c1b250715fa3888
Joseph Kokenge <jkokenge@gmail.com>
2015-08-11 16:24:16 -0500

    fixed .gitignore to suppress exlcusion of some files

---
+++ myproject/insps/lib/import_estabs.py
@@ -0,0 +1,45 @@
+import urllib2
+import csv
+import datetime
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+from insps.models import GeocodedEstab
+
+BASEURL = 'https://s3.amazonaws.com/inspections-csvs/'
+
+def make_today():
+    today = datetime.datetime.now().strftime("%m_%d_%y")
+    return today
+
+def load(reader_o):
+    estabs = GeocodedEstab.objects.all()
+    initial_estab_count = len(estabs)
+    print('Initial estab count %s') % initial_estab_count 
+
+    for o in reader_o:
+        record = GeocodedEstab(**o)
+        record_exists = estabs.filter(estab_id=record.estab_id).exists()
+        if record_exists:
+            print("record exists")
+        else:
+            record.save()
+            print("record saved")
+
+    estabs = GeocodedEstab.objects.all()
+    final_estab_count = len(estabs)
+    print('Final estab count %s') % final_estab_count 
+
+
+
+def get_csv(filename):
+    # url = BASEURL + make_today() + '/' + filename
+    url = BASEURL + "08_02_15" + '/' + filename
+
+    response = urllib2.urlopen(url)
+    reader_o = csv.DictReader(response)
+    return load(reader_o)
+
+
+def load_csv():
+    get_csv('estabs_tbl.csv')
---
+++ myproject/insps/lib/import_inspections.py
@@ -0,0 +1,45 @@
+import urllib2
+import csv
+import datetime
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+from insps.models import Inspection
+
+BASEURL = 'https://s3.amazonaws.com/inspections-csvs/'
+
+def make_today():
+    today = datetime.datetime.now().strftime("%m_%d_%y")
+    return today
+
+def load(reader_o):
+    inspections = Inspection.objects.all()
+    inspections_length = len(inspections)
+    print("Initial inspection length:%s") % inspections_length
+
+    for o in reader_o:
+        record = Inspection(**o)
+        record_exists = inspections.filter(inspection_key=record.inspection_key).exists()
+        if record_exists:
+            print("record exists")
+
+        else:
+            print("new record")
+            record.save() 
+
+    inspections = Inspection.objects.all()
+    final_inspections_length = len(inspections)
+    print("Final inspection length: %s") % final_inspections_length
+
+
+def get_csv(filename):
+    # url = BASEURL + make_today() + '/' + filename
+    url = BASEURL + "08_02_15" + '/' + filename
+
+    response = urllib2.urlopen(url)
+    reader_o = csv.DictReader(response)
+    return load(reader_o)
+
+
+def load_csv():
+    get_csv('inspections_tbl.csv')
