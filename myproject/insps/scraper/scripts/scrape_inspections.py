9c05af10b17f4d5edf4eb0607afa484e4a506a88
Joseph Kokenge <jkokenge@gmail.com>
2016-02-05 17:58:48 -0600

    uncommented the lines to clear data dir

--- myproject/insps/scraper/scripts/scrape_inspections.py
+++ myproject/insps/scraper/scripts/scrape_inspections.py
@@ -8,8 +8,8 @@ DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data/')
 
 def initiate_scrape():
     print('initiate scrape')  
-    #print('clearing out contents of data dir, if necessary...')   
-    #runshutil.rmtree(DATA_DIR)
+    print('clearing out contents of data dir, if necessary...')   
+    runshutil.rmtree(DATA_DIR)
     print('begin fetching pages')
     fetch_pages()
     get_estab_pages()    
